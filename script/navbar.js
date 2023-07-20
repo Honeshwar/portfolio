0ef312ebb7c56784372518f5da75f55f26ad56a3
Honeshwar Thakur <st566654@gmail.com>
2023-07-20 11:44:55 +0530

    add skill section

--- script/navbar.js
+++ script/navbar.js
@@ -1,35 +1,13 @@
-// hamburg / navbar
-let navbarOpen = false;
-$('#hamburg a').click(function (e) { 
-    e.preventDefault();
-
-    if(navbarOpen){
-        navbarOpen=false;
-        $('.navbar ul').css('display','none');
-        return;
-    }else{
-    $('.navbar ul').css({ 
-        width: "100%",
-        textAlign: "center",
-        display: "flex",
-        flexDirection: "column",
-        padding: "30px 0px"
-        });
-        navbarOpen=true;
-    }
-
-   
+// hamburg 
+$('#hamburg').on('click',function(){
+    $('#hamburg').css("display","none");
+    $('#hamburg-close').css("display","flex");
+})
+$('#hamburg-close').on('click',function(){
+    $('#hamburg-close').css("display","none");
+    $('#hamburg').css("display","flex");
 });
 
-// window.click((function () { 
-//     if( $('.hidden').css('display')==="none"){
-//      console.log("resize");
-//      $('.navbar ul').css({
-//          display: "flex",
-//          flexDirection: "row",
-//          });
-//     }
-//  }), 100);
 
 
 
