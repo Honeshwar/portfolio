6af6ee677eb89fb9a804649e6385002b1dce6094
Honeshwar Thakur <st566654@gmail.com>
2026-02-04 17:56:26 +0530

    remove unnecessary code

---
+++ script/navbar.js
@@ -0,0 +1,24 @@
+// hamburg 
+$('#hamburg').on('click',function(){
+    $('#hamburg').css("display","none");
+    $('#hamburg-close').css("display","flex");
+})
+$('#hamburg-close').on('click',function(){
+    $('#hamburg-close').css("display","none");
+    $('#hamburg').css("display","flex");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
